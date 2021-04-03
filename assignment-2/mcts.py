023793bac13043dcabfb708f16a6885a65a626af
andersbennaes <anders@bennaes.com>
2021-04-04 00:53:57 +0200

    MCTS logic. Search and node expansion

--- assignment-2/mcts.py
+++ assignment-2/mcts.py
@@ -2,34 +2,55 @@ from dataclasses import dataclass, field
 
 import numpy as np
 
-
 @dataclass
 class MCNode:
-    player: int = 0
-    reward: int = 0
-    visit_count: int = 0
-    is_leaf: bool = False
-    successors: dict = field(default_factory=dict)
+    def __init__(self, state):
+        self.state = state
+        #Var ikkje init metode her, visste ikkje om det var med vilje eller ikkje.  La til state
+        player: int = 0
+        reward: int = 0
+        visit_count: int = 0
+        is_leaf: bool = False
+        successors: dict = field(default_factory=dict)
 
 
 class MCTree:
-    def __init__(self, root, tree_policy, target_policy):
+    def __init__(self, root, tree_policy=1, target_policy):
         self.root = root
         self.tree_policy = tree_policy          # Used for tree traversal (which is usually highly exploratory)
         self.target_policy = target_policy      # Used for rollout simulation (default policy)
         ...
 
-    def search(self):
+    def search(self, env):
         """ (1) - Tree Search
         Traversing the tree from the root to a leaf node by using the tree policy.
         """
-        return True
+        search_game = env.copy()
+        cur_node = self.root            
+        while not cur_node.is_leaf: 
+            # assuming that tree policy is the c constant in the uct formula
+            best_action= np.argmax([self.uct(cur_node, action, self.tree_policy) for action in cur_node])            
+            search_game.move(best_action, cur_node.player) #Riktig? er player i ein node player som har neste move ut av denna nodens state?
+            cur_node = cur_node.successors[best_action]
 
-    def node_expansion(self, node):
+        return cur_node, search_game
+
+
+    def node_expansion(self, node, search_game):
         """ (2)
         Generating some or all child states of a parent state, and then connecting the tree node housing
         the parent state (a.k.a. parent node) to the nodes housing the child states (a.k.a. child nodes).
         """
+        actions = search_game.get_legal_actions()
+        for action in actions:
+            game = search_game.copy()
+            game.move(action, node.player) #riktig player?
+            child = MCNode(game.get_observation()) # Vi må vel sette player når vi initialiserer noder? Den hentes vel ikkje fra get_observation
+            node.successors[action] = child
+
+        return node
+
+        
         ...
 
     def leaf_evaluation(self, node):
@@ -46,8 +67,13 @@ class MCTree:
         """
         while node is not None:
             node.score += score
+            node.visit_count += 1 #La til denne
             node = node.parent
 
+
+    def uct(self, node, action, c)
+        child_node = node.successors[action]
+        return child_node.reward + c*sqrt( log(node.visit_count) / (1 + child_node.visit_count))
 
 class Agent:
     ...
@@ -74,10 +100,17 @@ class MCTSAgent(Agent):
             mct = MCTree(root, self.tree_policy, self.target_policy)    #       (c) Initialize the Monte Carlo Tree (MCT) to a single root, which represents sinit 
 
             while not env_actual.is_terminal():                         #       (d) While Ba not in a final state:
-                env_sim = env_actual.copy()                             #           • Initialize Monte Carlo game board (Bmc) to same state as root. 
+                #env_sim = env_actual.copy()                             #           • Initialize Monte Carlo game board (Bmc) to same state as root. 
 
                 for sim in self.n_simulations:                          #           • For gs in number search games:
-                    leaf = mct.search()                                 #               – Use tree policy Pt to search from root to a leaf (L) of MCT. Update Bmc with each move.
+                    env_sim = env_actual.copy()
+                    leaf, search_game = mct.search(env_sim)             #               – Use tree policy Pt to search from root to a leaf (L) of MCT. Update Bmc with each move.
+                    #Expanding if leaf has been visited before
+                    if leaf.visit_count > 0:
+                        leaf = mct.node_expansion(leaf, search_game) 
+                        # Assuming first new child node chosen after expansion
+                        action = search_game.get_legal_actions()[0]
+                        leaf = leaf.successors[action]                    
                     reward = mct.rollout(leaf)                          #               – Use ANET to choose rollout actions from L to a final state (F). Update Bmc with each move. 
                     mct.backprop(leaf, reward)                          #               – Perform MCTS backpropagation from F to root.
                 
