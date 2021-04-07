d2581a92464a1ac6e2f6931901c862fdfa07b91b
Patrik Kjærran <patrikkja@gmail.com>
2021-04-08 00:35:14 +0200

    Finish network implementation (TODO: custom grads)

--- assignment-2/network.py
+++ assignment-2/network.py
@@ -43,7 +43,7 @@ class ActorNetwork(Actor):
     
     @tf.function
     def __call__(self, state):
-        """Returns the Actors' evaluation of a given state."""
+        """Returns the networks evaluation of a given state (observation)."""
         decoded = self.env.decode_state(state)
         tensor = tf.convert_to_tensor(decoded)
         reshaped = tf.reshape(tensor, shape=(1, -1))
@@ -65,16 +65,39 @@ class ActorNetwork(Actor):
         output_spec = self.env.spec().actions
         model.add(tf.keras.layers.Dense(output_spec, activation='softmax'))
 
-        # Add custom transformation layer to modify logits
+        # TODO: Add custom transformation layer to modify logits
         # In this manner we might avoid gradient taping!
         # We will need to supply custom imput to the transformation layer
-        # TODO: Add custom layer here
+        # Can assign a custom gradient function to the custom layer
         # Might solve this if using tf.sparse functions which normalize
         # using missing values in labels
         
         # Compile model
         model.compile(
             optimizer=self.optimizer(learning_rate=self.alpha),
-            loss=tf.keras.losses.SparseCategoricalCrossentropy()
+            loss=tf.keras.losses.KLDivergence()
         )
         return model
+
+    @tf.function
+    def update(self, observations, labels):
+        # Wraps tf.ops to record operations such that gradients can be calculated
+        with tf.GradientTape() as tape:
+            # Forward propagate
+            logits = self(observations)
+            labels = tf.stop_gradient(labels)
+
+            # TODO: Modify logits by masking prohibited actions
+
+            # Calculate loss
+            # TODO: Update using 'self.model.loss' after model.compile(...)
+            # From TF source code https://github.com/tensorflow/tensorflow/blob/v2.4.1/tensorflow/python/keras/engine/training.py#L449-L549
+            loss = tf.nn.softmax_cross_entropy_with_logits(
+                logits=logits, labels=labels
+            )
+
+        # Compute gradients.
+        weights = self.model.trainable_weights
+        gradients = tape.gradient(loss, weights)
+        self.model.optimizer.apply_gradients(zip(gradients, weights))
+        return loss
